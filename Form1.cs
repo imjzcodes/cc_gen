b0cc2dacc6b76a70a02e14e9845079769ac7ddad
JZ <riodayoungoglover@proton.me>
2024-12-18 17:59:41 -0800

    Added main source

---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CC_GEN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.MaximizeBox = false;
+            this.MinimumSize = this.Size;
+            this.MaximumSize = this.Size;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cards = CardGenerator.Gen(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox3.Text), textBox2.Text.ToString());
+            richTextBox1.Text = cards;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Create a SaveFileDialog to get the file path from the user
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files|*.txt|All Files|*.*";
+            saveFileDialog.DefaultExt = "txt";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Get the selected file path
+                string filePath = saveFileDialog.FileName;
+
+                // Save the text to the file
+                File.WriteAllText(filePath, richTextBox1.Text);
+            }
+        }
+    }
+}
---
+++ generator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+public class CardGenerator
+{
+    public static string Gen(long baseNumber, int amount, string separator)
+    {
+        if (baseNumber < 100000 || baseNumber > 999999)
+        {
+            throw new ArgumentException("Base number must be a 6-digit number.");
+        }
+
+        Random random = new Random();
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < amount; i++)
+        {
+            // Generate card number
+            long cardNumber = baseNumber * 10000000000L +
+                              (long)random.Next(100000, 1000000) * 10000L +
+                              random.Next(10000);
+
+            // Generate expiration date (mm/yyyy)
+            int month = random.Next(1, 13);
+            int year = random.Next(2026, 2036);
+
+            // Generate CVV
+            int cvv = random.Next(100, 1000);
+
+            // Append to result
+            result.Append(cardNumber.ToString("D16"))
+                  .Append(separator)
+                  .Append($"{month:D2}/{year}")
+                  .Append(separator)
+                  .Append(cvv.ToString("D3"));
+
+            if (i < amount - 1)
+            {
+                result.AppendLine();
+            }
+        }
+
+        return result.ToString();
+    }
+}
